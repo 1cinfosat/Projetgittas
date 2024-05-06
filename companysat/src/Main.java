3dd1139e7ef0446240ff1782666dcbcc9f9ac6d3
ProjetGitSAT <esprit.git.1cinfo1@gmail.com>
2024-05-06 01:04:23 +0100

    create object entreprise and decalare data in list

--- companysat/src/Main.java
+++ companysat/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String [] args) {
-        System.out.println("Hello world!");
+        //var
+        Entreprise entreprise = new Entreprise();
+
+        //Data
+        entreprise.prestations.add(new Prestation(1, "Bolt App", "Bolt Inc"));
+        entreprise.prestations.add(new Service(2, "Formation Flutter", "Orange Tunisie", 36, 100));
+        entreprise.prestations.add(new Service(3,"Consulting Architecture", "Avaxia", 87, 150));
+        entreprise.prestations.add(new Produit(4, "E-learning Platform", "MSB", 15200, "12/2022"));
+        entreprise.prestations.add(new Produit(5,"Social Network App", "Orange Tunisie", 8800, "03/2023"));
+        entreprise.prestations.add(new Produit(6,"Scanner de cheque", "BNA", 12600, "12/2023"));
     }
 }
