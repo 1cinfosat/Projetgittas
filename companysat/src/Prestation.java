e696fed9690cc00ccfe2982b1ed41e51f10f1953
freeayoub <gayoub2009@gmail.com>
2024-05-07 06:13:08 +0100

    ajouter un Constructeur paramétré

--- companysat/src/Prestation.java
+++ companysat/src/Prestation.java
@@ -4,4 +4,15 @@ public sealed class Prestation permits Service, Produit {
     //var
     protected int id;
     protected String nom_projet, nom_client;
+
+    public Prestation() {
+    }
+
+    public Prestation(int id, String nom_projet, String nom_client) {
+        this.id = id;
+        this.nom_projet = nom_projet;
+        this.nom_client = nom_client;
+    }
+
+    
 }
