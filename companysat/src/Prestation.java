1b6adc276b1a9d22960dcc4c047de43535dc9f7c
freeayoub <gayoub2009@gmail.com>
2024-05-07 19:34:30 +0100

    add getter and setter for prestation

--- companysat/src/Prestation.java
+++ companysat/src/Prestation.java
@@ -13,6 +13,28 @@ public sealed class Prestation permits Service, Produit {
         this.nom_projet = nom_projet;
         this.nom_client = nom_client;
     }
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom_projet() {
+        return nom_projet;
+    }
+
+    public void setNom_projet(String nom_projet) {
+        this.nom_projet = nom_projet;
+    }
+
+    public String getNom_client() {
+        return nom_client;
+    }
+
+    public void setNom_client(String nom_client) {
+        this.nom_client = nom_client;
+    }
     
 }
