65d20bed11b194eaed77e2733ec48fc5f36ce487
itsschebbi <unk1v9@gmail.com>
2024-05-08 13:36:46 +0200

    add getters

--- companysat/src/Service.java
+++ companysat/src/Service.java
@@ -1,5 +1,31 @@
 import java.util.Objects;
 
 public class Service extends Prestation{
+//var
+    private int nombre_heures;
+    private int prix_heure;
+    //con
+    public Service(){
+
+    }
+    public Service(int id, String nom_projet, String nom_client, int prix_heure, int nombre_heures){
+        super(id, nom_projet, nom_client);
+        this.prix_heure = prix_heure;
+        this.nombre_heures = nombre_heures;
+    }
+
+    public int getNombre_heures() {
+        return nombre_heures;
+    }
+
+
+    public int getPrix_heure() {
+        return prix_heure;
+    }
+
+    
+  
+
+   
 
 }
